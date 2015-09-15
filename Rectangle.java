251ee77d1a781ecd59070d833a0d654cb334d6b8
unknown <bbm204@AN112507-CT103.nvstu.edu>
2015-09-15 17:28:25 -0400

    add Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+			String material = "";
+			        double width = 0.0;
+				        double height = 0.0;
+					        double perimeter = 0.0;
+						        double area = 0.0;
+							        Scanner input = new Scanner(System.in);
+										Scanner keyboard = new Scanner (System.in);
+													System.out.println("Enter width");
+																	width = input.nextDouble();
+																						System.out.println("Enter height");
+																												height = input.nextDouble();
+																																		
+																																								double calculation = width+width+height+height;	
+																																														double calculation2 = height*width;
+																																																			
+																																																									   System.out.println("The Perimeter is: " + calculation + "\n" + 
+																																																									   				                    "The Area is: " + calculation2 + "");
+																																																															    	}
+
+																																																																}
+
